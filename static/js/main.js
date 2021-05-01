916f6c3dd4a0bbdde1d6a46857a387892fab36be
Tony Pham <mrphamtony@gmail.com>
2021-04-30 19:00:11 -0700

    transition iteration

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,14 @@
+function generateAPI(length) {
+    var result = [];
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for (var i = 0; i < length; i++) {
+        result.push(characters.charAt(Math.floor(Math.random() *
+            charactersLength)));
+    }
+    return result.join('');
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
 
