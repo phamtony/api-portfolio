cacf7cf05483936b42e46c46b49022c9bd15872d
Tony Pham <mrphamtony@gmail.com>
2021-05-02 21:34:48 -0700

    postgresql hack

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config(object):
 
     UPLOAD_FOLDER = './static/images/'
     SECRET_KEY = os.environ.get("SECRET_KEY")
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///portfolio.db")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL").replace("://", "ql://", 1)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     SESSION_COOKIE_SECURE = True
