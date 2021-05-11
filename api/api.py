98dce0b2a6114eba1f6579ab0cbc68b970d33706
Tony Pham <mrphamtony@gmail.com>
2021-05-10 20:08:41 -0700

    model and api alteration refactor

--- api/api.py
+++ api/api.py
@@ -10,17 +10,41 @@ json_return = Blueprint("json_return", __name__)
 def api():
     api = request.args.get("api")
     id = General.query.filter_by(api_key=api).first().id
+
     skill_query = Skills.query.filter_by(general_id=id).first()
     about_query = About.query.filter_by(general_id=id).first()
     experience_query = Experience.query.filter_by(general_id=id)
     education_query = Education.query.filter_by(general_id=id)
     project_query = Project.query.filter_by(general_id=id)
 
-    general = General.query.get(id).to_dict()
-    skills = skill_query.to_dict() if skill_query else {}
-    about = about_query.to_dict() if about_query else {}
-    experience = [experience.to_dict() for experience in experience_query] if experience_query else {}
-    education = [ed.to_dict() for ed in education_query] if education_query else {}
-    projects = [project.to_dict() for project in project_query] if project_query else {}
+    try:
+        general = General.query.get(id).serialized
+    except AttributeError:
+        general = {}
+
+    try:
+        skills = skill_query.serialized
+    except AttributeError:
+        skills = {}
+
+    try:
+        about = about_query.serialized
+    except AttributeError:
+        about = {}
+
+    try:
+        experience = [experience.serialized for experience in experience_query]
+    except AttributeError:
+        experience = {}
+
+    try:
+        education = [ed.serialized for ed in education_query]
+    except AttributeError:
+        education = {}
+
+    try:
+        projects = [project.serialized for project in project_query]
+    except AttributeError:
+        projects = []
 
     return jsonify(skills=skills, general=general, about=about, experience=experience, education=education, projects=projects)
--- models.py
+++ models.py
@@ -3,15 +3,8 @@ from flask_login import UserMixin
 
 from extensions import db
 
-class ModelFunc():
-    def __init__(self):
-        super().__init__()
 
-    def to_dict(self):
-        return {column.name: getattr(self, column.name) for column in self.__table__.columns if column.name != "password"}
-
-
-class General(db.Model, ModelFunc, UserMixin):
+class General(db.Model, UserMixin):
     __tablename__ = "general"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
@@ -29,8 +22,22 @@ class General(db.Model, ModelFunc, UserMixin):
     about = relationship("About", uselist=False, back_populates="general")
     skills = relationship("Skills", uselist=False, back_populates="general")
 
+    def __repr__(self):
+        return "<General(name='%s', occupation='%s', email='%s', github='%s', linkedin='%s', user_intro='%s')>" % (self.name, self.occupation, self.email, self.github, self.linkedin, self.user_intro)
 
-class About(db.Model, ModelFunc):
+    @property
+    def serialized(self):
+        return {
+            "name": self.name,
+            "occupation": self.occupation,
+            "email": self.email,
+            "github": self.github,
+            "linkedin": self.linkedin,
+            "user_intro": self.user_intro,
+        }
+
+
+class About(db.Model):
     __tablename__ = "about"
     id = db.Column(db.Integer, primary_key=True)
     intro = db.Column(db.String(1000))
@@ -39,8 +46,18 @@ class About(db.Model, ModelFunc):
     general_id = db.Column(db.Integer, db.ForeignKey("general.id"))
     general = relationship("General", back_populates="about")
 
+    def __repr__(self):
+        return "<About(intro='%s', image='%s')>" % (self.intro, self.image)
 
-class Experience(db.Model, ModelFunc):
+    @property
+    def serialized(self):
+        return {
+            "intro": self.intro,
+            "image": self.image,
+        }
+
+
+class Experience(db.Model):
     __tablename__ = "experience"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
@@ -52,8 +69,21 @@ class Experience(db.Model, ModelFunc):
     general_id = db.Column(db.Integer, db.ForeignKey("general.id"))
     general = relationship("General", back_populates="experience")
 
+    def __repr__(self):
+        return "<Experience(name='%s', position='%s', time='%s', link='%s', description='%s')>" % (self.name, self.position, self.time, self.link, self.description)
 
-class Education(db.Model, ModelFunc):
+    @property
+    def serialized(self):
+        return {
+            "name": self.name,
+            "position": self.position,
+            "time": self.time,
+            "link": self.link,
+            "description": self.description,
+        }
+
+
+class Education(db.Model):
     __tablename__ = "education"
     id = db.Column(db.Integer, primary_key=True)
     school = db.Column(db.String(150))
@@ -63,8 +93,19 @@ class Education(db.Model, ModelFunc):
     general_id = db.Column(db.Integer, db.ForeignKey("general.id"))
     general = relationship("General", back_populates="education")
 
+    def __repr__(self):
+        return "<Education(school='%s', time='%s', degree='%s')>" % (self.school, self.time, self.degree)
 
-class Skills(db.Model, ModelFunc):
+    @property
+    def serialized(self):
+        return {
+            "school": self.school,
+            "time": self.time,
+            "degree": self.degree,
+        }
+
+
+class Skills(db.Model):
     __tablename__ = "skills"
     id = db.Column(db.Integer, primary_key=True)
     language = db.Column(db.String(500))
@@ -75,8 +116,20 @@ class Skills(db.Model, ModelFunc):
     general_id = db.Column(db.Integer, db.ForeignKey("general.id"))
     general = relationship("General", back_populates="skills")
 
+    def __repr__(self):
+        return "<Skills(language='%s', framework_library='%s', database='%s', misc='%s')>" % (self.language, self.framework_library, self.database, self.misc)
 
-class Project(db.Model, ModelFunc):
+    @property
+    def serialized(self):
+        return {
+            "language": self.language,
+            "framework_library": self.framework_library,
+            "database": self.database,
+            "misc": self.misc,
+        }
+
+
+class Project(db.Model):
     __tablename__ = "project"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(250))
@@ -90,3 +143,17 @@ class Project(db.Model, ModelFunc):
     general_id = db.Column(db.Integer, db.ForeignKey("general.id"))
     # Create reference to the General object, the "project" refers to the project property in the General class.
     general = relationship("General", back_populates="project")
+
+    def __repr__(self):
+        return "<Project(name='%s', link='%s', github_link='%s', screenshot='%s', description='%s', tech_list='%s')>" % (self.name, self.link, self.github_link, self.screenshot, self.description, self.tech_list)
+
+    @property
+    def serialized(self):
+        return {
+            "name": self.name,
+            "link": self.link,
+            "github_link": self.github_link,
+            "screenshot": self.screenshot,
+            "description": self.description,
+            "tech_list": self.tech_list,
+        }
