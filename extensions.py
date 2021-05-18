ee8c6e82a3def9aded339b9dff4f437946119a49
Tony Pham <mrphamtony@gmail.com>
2021-05-18 14:52:27 -0700

    flash message for guest

--- extensions.py
+++ extensions.py
@@ -14,4 +14,4 @@ s3_session = boto3.session.Session()
 cors = CORS()
 
 login_manager.login_view = "register_login_page.login"
-login_manager.login_message = "You need to log in to view the page. If you're not registered, please register first."
+login_manager.login_message = "You need to log in to view the page. If you're not registered, please register first. <br><br> If you're here to just browse around and see how this works, please use these credentials: <br><br> Email: guest@guest.com <br> Password: guest"
